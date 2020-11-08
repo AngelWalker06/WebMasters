9a970dfe660e27a3f8a7b0176181e3788fb730ca
mpalad2 <71671886+mpalad2@users.noreply.github.com>
2020-11-07 20:47:06 -0500

    Update level2.php

--- project2/level2.php
+++ project2/level2.php
@@ -5,7 +5,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>level 1</title>
+    <title>Level 2</title>
     <style media="screen">
       .letterInput{
         width: 20px;
