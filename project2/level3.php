c2908aec64e0fad45a5d65eedf58bfe21397bd1b
Chul Chong <chulchong1030@gmail.com>
2020-11-07 20:50:17 -0500

    fixed minor issues

--- project2/level3.php
+++ project2/level3.php
@@ -1,6 +1,7 @@
-<?php session_save_path("session"); 
-      session_start();
-      $username = $_SESSION['username'];?>
+<?php session_save_path("session");
+session_start();
+$username = $_SESSION['username'];
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -19,10 +20,10 @@
   </head>
 
   <body>
-    <?php include("header.php") ?>
+    <?php include "header.php"; ?>
     <h1>Hello <?php echo $username; ?>!</h1>
     <h2>Level 3</h2>
-    <a href="#">Leaderboard</a>
+    <a href="leaderboard3.php">Leaderboard</a>
     <br>
     <br>
     <form action="" method="post">
@@ -30,44 +31,43 @@
       <legend>Find the correct word</legend>
       <p>A month</p>
       <?php
-      if ($_POST["first"]!="a"){
-        echo'<input type="text" name="first" class="letterInput" value="">';
-      }else if($_POST["reset"]){
-        echo'<input type="text" name="first" class="letterInput" value="">';
-      } else if ($_POST["first"]=="a"){
-        echo'<input type="text" name="first" class="letterInput correct" value="a">';
+      if ($_POST["first"] != "a") {
+        echo '<input type="text" name="first" class="letterInput" value="">';
+      } elseif ($_POST["reset"]) {
+        echo '<input type="text" name="first" class="letterInput" value="">';
+      } elseif ($_POST["first"] == "a") {
+        echo '<input type="text" name="first" class="letterInput correct" value="a">';
       }
 
-        if ($_POST["second"]!="p"){
-          echo'<input type="text" name="second" class="letterInput" value="">';
-        }else if($_POST["reset"]){
-          echo'<input type="text" name="second" class="letterInput" value="">';
-        } else if ($_POST["second"]=="p"){
-          echo'<input type="text" name="second" class="letterInput correct" value="p">';
-        }
-        if ($_POST["third"]!="r"){
-          echo'<input type="text" name="third" class="letterInput" value="">';
-        }else if($_POST["reset"]){
-          echo'<input type="text" name="third" class="letterInput" value="">';
-        } else if ($_POST["third"]=="r"){
-          echo'<input type="text" name="third" class="letterInput correct" value="r">';
-        }
-        if ($_POST["fourth"]!="i"){
-          echo'<input type="text" name="fourth" class="letterInput" value="">';
-        }else if($_POST["reset"]){
-          echo'<input type="text" name="fourth" class="letterInput" value="">';
-        } else if ($_POST["fourth"]=="i"){
-          echo'<input type="text" name="fourth" class="letterInput correct" value="i">';
-        }
-        if ($_POST["fifth"]!="l"){
-          echo'<input type="text" name="fifth" class="letterInput" value="">';
-        }else if($_POST["reset"]){
-          echo'<input type="text" name="fifth" class="letterInput" value="">';
-        } else if ($_POST["fifth"]=="l"){
-          echo'<input type="text" name="fifth" class="letterInput correct" value="l">';
-        }
-
-       ?>
+      if ($_POST["second"] != "p") {
+        echo '<input type="text" name="second" class="letterInput" value="">';
+      } elseif ($_POST["reset"]) {
+        echo '<input type="text" name="second" class="letterInput" value="">';
+      } elseif ($_POST["second"] == "p") {
+        echo '<input type="text" name="second" class="letterInput correct" value="p">';
+      }
+      if ($_POST["third"] != "r") {
+        echo '<input type="text" name="third" class="letterInput" value="">';
+      } elseif ($_POST["reset"]) {
+        echo '<input type="text" name="third" class="letterInput" value="">';
+      } elseif ($_POST["third"] == "r") {
+        echo '<input type="text" name="third" class="letterInput correct" value="r">';
+      }
+      if ($_POST["fourth"] != "i") {
+        echo '<input type="text" name="fourth" class="letterInput" value="">';
+      } elseif ($_POST["reset"]) {
+        echo '<input type="text" name="fourth" class="letterInput" value="">';
+      } elseif ($_POST["fourth"] == "i") {
+        echo '<input type="text" name="fourth" class="letterInput correct" value="i">';
+      }
+      if ($_POST["fifth"] != "l") {
+        echo '<input type="text" name="fifth" class="letterInput" value="">';
+      } elseif ($_POST["reset"]) {
+        echo '<input type="text" name="fifth" class="letterInput" value="">';
+      } elseif ($_POST["fifth"] == "l") {
+        echo '<input type="text" name="fifth" class="letterInput correct" value="l">';
+      }
+      ?>
 
       <br><br>
 
@@ -75,92 +75,95 @@
       <input type="submit" name="clues" value="clue"><br><br>
       <input type="submit" name="reset" value="restart">
       <br>
-      <?php
-      if(isset( $_POST["clues"])) {
+      <?php if (isset($_POST["clues"])) {
         echo '<p>The start of spring</p>';
-      }
-
-       ?>
-      <?php
-      if ($_POST["reset"]){
+      } ?>
+      <?php if ($_POST["reset"]) {
         file_put_contents("score_tracker.txt", 4);
-       }
-       ?>
+      } ?>
     </fieldset>
 
   </form>
 
 <div class="fail/pass">
 <?php
-  $file = 'score_tracker.txt';
-  $trials = intval(file_get_contents($file));
-  if ($trials == 0){
-    echo '<h3> Game Over. <h3>';
-    echo '<a href="index.php">Back to main page</a>';
-    file_put_contents($file, 4);
-
-  }
-  else{
-    if (isset($_POST['submit'])){
-      $finalWord="";
-      foreach ($_POST as $key => $value) {
-        $finalWord .= $value;
-      }
-      if ($finalWord=="aprilSubmit"){
+$file = 'score_tracker.txt';
+$trials = intval(file_get_contents($file));
+if ($trials == 0) {
+  echo '<h3> Game Over. <h3>';
+  echo '<a href="index.php">Back to main page</a>';
+  file_put_contents($file, 4);
+} else {
+  if (isset($_POST['submit'])) {
+    $finalWord = "";
+    foreach ($_POST as $key => $value) {
+      $finalWord .= $value;
+    }
+    if ($finalWord == "aprilSubmit") {
       echo 'Congratulations, you completed this level. <a href="level4.php">Next level</a>';
       $score = $trials * 2;
       //file_put_contents("users_score.txt", $score);
-	  
-		//added code
-		$valid = true;
-		//sets file equal to calling for the users file 
-		$file = file('userdata.txt');
-		//sets count equal to the the amount of lines (which is the same as the number of users) in the file
-		$count = count($file);
-		//set topFive equal to the top five of the user list
-		
-		//sets fullFile equal to all of the users' information within the file
-		$fullFile = file_get_contents('userdata.txt');
-		//sets the array into separate users per element/index
-		$users = explode("\n", $fullFile);
-		
-		//find user in text file to update by running through the whole file
-		for ($i=0;$i<count($users);$i++){
-			//splits string into each piece of info of the current user
-			$current = explode(",", $users[$i]);
-			//checks if current user is the user that is being updated
-			if ($current[0] == $username){
-				//create new string with updated score
-				$updateUser = $current[0].",".$current[1].",".$current[2].",".$score.",".
-								$current[4].",".$current[5].",".$current[6];
-				//replace string in the text file with new updated info
-				$fullFile = str_replace($users[$i],$updateUser,$fullFile);
-				//stop once found
-				break;
-			}
-		}
-		
-		//checks validity
-		if ($valid == true){
-			//adds the user's info into the existing users list
-			file_put_contents('userdata.txt',$fullFile);
-		}
-		file_put_contents($file, 4);
-      }
-     else{
-      echo 'You must complete this level to move on';
-      $trials=$trials-1;
-      $input=$trials."\n";
-      file_put_contents($file, $input);
 
-    }
+      //added code
+      $valid = true;
+      //sets file equal to calling for the users file
+      $file = file('userdata.txt');
+      //sets count equal to the the amount of lines (which is the same as the number of users) in the file
+      $count = count($file);
+      //set topFive equal to the top five of the user list
+
+      //sets fullFile equal to all of the users' information within the file
+      $fullFile = file_get_contents('userdata.txt');
+      //sets the array into separate users per element/index
+      $users = explode("\n", $fullFile);
+
+      //find user in text file to update by running through the whole file
+      for ($i = 0; $i < count($users); $i++) {
+        //splits string into each piece of info of the current user
+        $current = explode(",", $users[$i]);
+        //checks if current user is the user that is being updated
+        if ($current[0] == $username) {
+          //create new string with updated score
+          $updateUser =
+            $current[0] .
+            "," .
+            $current[1] .
+            "," .
+            $current[2] .
+            "," .
+            $score .
+            "," .
+            $current[4] .
+            "," .
+            $current[5] .
+            "," .
+            $current[6];
+          //replace string in the text file with new updated info
+          $fullFile = str_replace($users[$i], $updateUser, $fullFile);
+          //stop once found
+          break;
+        }
+      }
+
+      //checks validity
+      if ($valid == true) {
+        //adds the user's info into the existing users list
+        file_put_contents('userdata.txt', $fullFile);
+      }
+      file_put_contents($file, 4);
+    } else {
+      echo 'You must complete this level to move on';
+      $trials = $trials - 1;
+      $input = $trials . "\n";
+      file_put_contents($file, $input);
     }
   }
- ?>
+}
+?>
  <p>Number of tries remaining: <?php echo $trials; ?></p>
 
 </div>
-<?php include("footer.php") ?>
+<?php include "footer.php"; ?>
 
 
   </body>
