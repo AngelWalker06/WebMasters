2313d778c0c211bc4e7ff7fff63994f6791d35d5
Chul Chong <chulchong1030@gmail.com>
2020-12-02 21:28:06 -0500

    added scroing system

--- Project3/gamescript.js
+++ Project3/gamescript.js
@@ -813,6 +813,10 @@ function checkWin() {
     document.getElementById("gameWin").innerHTML =
       "Game Win! The puzzle is completed!";
     gameWin = false;
+    $(document).ready(function () {
+      // Show the Modal on load
+      $("#modalOn").modal("show");
+    });
   }
 }
 
@@ -970,4 +974,39 @@ function dropConfetti() {
   const confs = new Array(confNum).fill().map((_) => new Confetti());
 
   loop();
+}
+function scoreSubmit() {
+  var name = document.getElementById("name").value;
+  var setTime = document.getElementById("time").value;
+  var setMove = document.getElementById("move").value;
+
+  var currentBestTime = localStorage.getItem(name);
+  var currentBestMove = localStorage.getItem(name + "move");
+
+  if (setTime > currentBestTime) {
+    localStorage.setItem(name, setTime);
+    setTime = 0;
+  } else {
+    localStorage.setItem(name, currentBestTime);
+  }
+
+  if (setMove > currentBestMove) {
+    localStorage.setItem(name + "move", setMove);
+    setMove = 0;
+  } else {
+    localStorage.setItem(name + "move", currentBestMove);
+  }
+
+  var showBestTime = localStorage.getItem(name);
+  var showBestMove = localStorage.getItem(name + "move");
+
+  alert(
+    "Congrats!" +
+      name +
+      "'s best time is " +
+      showBestTime +
+      " and best move is " +
+      showBestMove +
+      "."
+  );
 }
